60a259be45e9e1cc7d624caf04b90e7489086620
initial commit <prajeeshkk@Prajeeshs-Mac-mini.local>
2015-06-05 14:30:48 +0530

    new styled version

---
+++ js/nav.js
@@ -0,0 +1,43 @@
+$(document).ready(function()
+{
+	$('.MenuIcon').click(function()
+	{
+		$('.navigate').toggle().addClass('open');
+	});
+	$('.navigate > .menu-item-has-children').each(function()
+	{
+		$(this).prepend('<span class="touch"></span>');
+
+		$(this).find('> span').click(function() {
+			$(this).parent().find('> ul').toggle();
+		});
+	});
+	$('.sub-menu .sub-menu').each(function()
+	{
+		$(this).parents('li').addClass('miniChild');
+	});
+	$width = $( document ).width();
+	if($width>800)
+	{
+		$('.sub-menu .menu-item-has-children a').hover(function()
+		{
+			$(this).closest('.item-with-ul').find('ul').toggle();
+			
+		});	
+	 }
+	$('.sub-menu .menu-item-has-children').each(function()
+	{
+		//$(this).find('a:not(.sub-menu .sub-menu a)').prepend('<span class="plus"></span>');
+		$(this).prepend('<span class="plus"></span>');
+	});
+
+	$('.sub-menu .menu-item-has-children').each(function()
+	{
+		$(this).find('> span').click(function() {
+			$(this).parent().find('> ul').toggle();
+		});
+	});
+});
+
+
+
